ecc7b54a5c6bfb2af6682a833d5358b8ffcb0c9d
xuehaiping <claymoresq@gmail.com>
2018-02-06 15:07:44 -0600

    Added global .gitignore, HW1 and P1 with stat files.

---
+++ HW1/task1/constants.py
@@ -0,0 +1,4 @@
+host = "34.233.78.56"
+db = "fitness_X"
+collection = "fitness_X"
+
---
+++ HW1/task1/queries.py
@@ -0,0 +1,32 @@
+from mongo_connect import connectMongo
+import constants
+import pymongo
+import json
+import pprint
+
+
+collection = connectMongo()
+
+##### FIND ALL ENTRIES IN THE DATABASE #####
+# Assuming RQ0 is the query to find all entries in the database
+RQ0 = collection.find()
+for data in RQ0:
+	pprint.pprint(data)
+
+######## FIND ENTRIES WITH CONDITION #######
+######## collection.find(CONDITION) #######
+######## E.g., collection.find({"Name" : "Alice"}) #######
+
+######## UPDATE ENTRIES WITH CONDITION ########
+######## collection.update_one(CONDITION, _update_) #######
+######## collection.update_many(CONDITION, _update_)
+######## E.g., collection.find({"Name" : "Alice"}, {"$inc" : {"age" : 1} })
+
+######## DELETE ENTRIES WITH CONDITION ########
+######## collection.delete_one(CONDITION) #######
+######## collection.delete_many(CONDITION)
+######## E.g., collection.find({"Name" : "Alice"})
+
+######## AGGREGATE ENTRIES WITH PIPELINE ########
+######## collection.aggregate(PIPELINE) ########
+
